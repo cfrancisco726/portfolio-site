2b29988374c62064f985da2f4f19b6df674d112d
cfrancisco726 <cfrancisco726@gmail.com>
2018-05-02 21:18:00 -0400

    added background and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Header from './Header';
+import About from './About';
+import Projects from './Projects';
+import Skills from './Skills';
+import Experience from './Experience';
 import './App.css';
 
 class App extends Component {
 	render() {
-		return <div>
-        <h1>hello</h1> 
-      </div>;
+		return (
+			<div>
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/projects" component={Projects} />
+						<Route exact path="/skills" component={Skills} />
+						<Route exact path="/experience" component={Experience} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
 	}
 }
-
 export default App;
---
+++ src/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Background from './images/bg_img.jpg';
+
+class Landing extends Component {
+	render() {
+		return <div> hello</div>;
+    {Background}
+	}
+}
+
+export default Landing;
