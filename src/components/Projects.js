87467c03e49fa705ca662762fdbe3d6946f13924
cfrancisco726 <cfrancisco726@gmail.com>
2018-08-06 16:17:52 -0400

    adding sample project 2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,39 +13,70 @@ class Projects extends Component {
 						<button className="btn modal-close">&times;</button>
 					</div>
 					<p className="modal-title">Projects</p>
-
-					<p>
-						Mail Monkey allows users to send and track surveys via email. A
-						profile page displays a history of the emails sent, along with a bar
-						chart of how their customers responded.{' '}
-					</p>
-					<p>
-						<strong>Built with:</strong> React Redux, MongoDB, Express and
-						Node.js.<br />
-						<strong>APIs and Services:</strong> Google Oauth, Stripe and
-						Sendgrid.
-					</p>
-					<div className="project-img">
-						<a href="https://surveyapps726.herokuapp.com" target="_blank">
-							<img
-								src={require('../images/mail_monkey_img.png')}
-								alt="projects"
-								width="80%"
-								padding-top="20%"
-							/>
-						</a>
-					</div>
-					<div className="project-links">
+					<div className="project-1">
 						<p>
-							<a href="https://surveyapps726.herokuapp.com" target="_blank">
-								LIVE DEMO
-							</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a
-								href="https://github.com/cfrancisco726/MailMonkey"
-								target="_blank"
-							>
-								GITHUB
-							</a>
+							Mail Monkey allows users to send and track surveys via email. A
+							profile page displays a history of the emails sent, along with a
+							bar chart of how their customers responded.{' '}
 						</p>
+						<p>
+							<strong>Built with:</strong> React Redux, MongoDB, Express and
+							Node.js.<br />
+							<strong>APIs and Services:</strong> Google Oauth, Stripe and
+							Sendgrid.
+						</p>
+						<div className="project-img">
+							<a href="https://surveyapps726.herokuapp.com" target="_blank">
+								<img
+									src={require('../images/mail_monkey_img.png')}
+									alt="projects"
+									width="80%"
+									padding-top="20%"
+								/>
+							</a>
+						</div>
+						<div className="project-links">
+							<p>
+								<a href="https://surveyapps726.herokuapp.com" target="_blank">
+									LIVE DEMO
+								</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a
+									href="https://github.com/cfrancisco726/MailMonkey"
+									target="_blank"
+								>
+									GITHUB
+								</a>
+							</p>
+						</div>
+						<div className="project-2">
+							<p>
+								Vinyl Records is a content management system that allows users to purchase records and  admin to update the content.
+							</p>
+							<p>
+								<strong>Built with:</strong> React Redux, MongoDB, Express and
+								Node.js.<br />
+							</p>
+							<div className="project-img">
+								<a href="http://recordstorec.rn8d3he9wv.us-east-1.elasticbeanstalk.com/" target="_blank">
+									<img
+										src={require('../images/vinyl_records_sample.png')}
+										alt="projects"
+										width="80%"
+										padding-top="20%"
+									/>
+								</a>
+							</div>
+							<div className="project-links">
+								<p>
+									<a href="http://recordstorec.rn8d3he9wv.us-east-1.elasticbeanstalk.com/" target="_blank">
+										LIVE DEMO
+									</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a
+										href="https://github.com/cfrancisco726/Record_Store_CMS"
+										target="_blank"
+									>
+										GITHUB
+									</a>
+								</p>
+						</div>
 					</div>
 				</div>
 			</div>
