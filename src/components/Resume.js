aeca144709d8683c0a3025c0cc80d12a4abd3578
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2021-09-01 10:37:40 -0400

    update resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Skills extends Component {
-	render() {
-		return (
-        <figure className="icon">
-            <a
-                href="https://drive.google.com/file/d/1m0YpbXZ8yTeCuZAsbhQd78_GVBywDH_6/view?usp=sharing"
-                rel="noopener noreferrer"
-                target="_blank"
-            >
-                <img
-                    src={require('../images/resume.svg')}
-                    alt="resume"
-                    width="40%"
-                />
-                <figcaption>resume</figcaption>
-            </a>
-        </figure>
-		);
-	}
+  render() {
+    return (
+      <figure className="icon">
+        <a
+          href="https://drive.google.com/file/d/16uzO1dw5KTkFkOnzbr2fmHeBpb2-5fgi/view?usp=sharing"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={require("../images/resume.svg")} alt="resume" width="40%" />
+          <figcaption>resume</figcaption>
+        </a>
+      </figure>
+    );
+  }
 }
 
 export default Skills;
