98286cfe59ca4fa8340d076d2c2ac5750c9229a4
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2022-01-24 15:36:17 -0500

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,71 +1,80 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Skills extends Component {
-	render() {
-		return (
-			<div id="modal3" className="modal">
-				<div className="modal-content">
-					<div class=" right-align">
-						<button className="btn modal-close">&times;</button>
-					</div>
-					<p className="modal-title">Skills</p>
-					<ul className="devicons">
-						<li>
-							<i className="devicon-javascript-plain" />
-						</li>
-						<li>
-							<i className="devicon-react-original-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-jquery-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-html5-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-css3-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-nodejs-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-ruby-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-mysql-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-postgresql-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-git-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-github-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-heroku-original-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-amazonwebservices-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-bootstrap-plain-wordmark" />
-						</li>
-						<li>
-							<i className="devicon-jasmine-plain-wordmark" />
-						</li>
-						<li>
-							<i className="fab fa-php"></i>
-						</li>
-						<li>
-							<i class="fab fa-wordpress"></i>
-						</li>
-					</ul>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="modal3" className="modal">
+        <div className="modal-content">
+          <div class=" right-align">
+            <button className="btn modal-close">&times;</button>
+          </div>
+          <p className="modal-title">Skills</p>
+          <ul className="devicons">
+            <li>
+              <i className="devicon-html5-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-css3-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-javascript-plain" />
+            </li>
+            <li>
+              <i className="devicon-react-original-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-jquery-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-nodejs-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-express-original-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-nextjs-original"></i>
+            </li>
+            <li>
+              <i className="devicon-ruby-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-mongodb-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-mysql-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-postgresql-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-git-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-github-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-amazonwebservices-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-sass-original" />
+            </li>
+            <li>
+              <i className="devicon-bootstrap-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-tailwindcss-original-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-foundation-plain-wordmark" />
+            </li>
+            <li>
+              <i className="devicon-jest-plain" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Skills;
