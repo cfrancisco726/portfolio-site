18e681830a154fd6bfede7756c9ac0124809f320
Carlo Francisco <carlofrancisco@Carlos-MacBook-Pro.local>
2022-03-05 14:11:07 -0500

    resume with bike shop added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Skills extends Component {
   render() {
     return (
       <figure className="icon">
         <a
-          href="https://drive.google.com/file/d/1TvJQtmwjfh8IaWYkHfYTDt1Wy75Qfzpv/view?usp=sharing"
+          href="https://drive.google.com/file/d/1WthqV0NWpFjRcuMwMXtqrZz07MLdGoiR/view?usp=sharing"
           rel="noopener noreferrer"
           target="_blank"
         >
-          <img src={require("../images/resume.svg")} alt="resume" width="40%" />
+          <img src={require('../images/resume.svg')} alt="resume" width="40%" />
           <figcaption>resume</figcaption>
         </a>
       </figure>
